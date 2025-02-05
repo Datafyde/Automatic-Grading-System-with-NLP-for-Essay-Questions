c5684665d70d297f76ae382b0dd5a8c3227e3581
Yomi Aledare <yomi.aledare@gmail.com>
2025-02-05 20:02:19 +0100

    Create main.py
    this is for the preliminary testing of the libraries and their functionalities

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math as m
+import spacy
+
+nlp = spacy.load('en_core_web_lg')
+
+st.title('Using Streamlit with Spacy')
+#score = st.slider('Select Mark', 2, 5, 3)
+ans = st.text_input('Type Answer key:')
+sub = st.text_input('Type Submission:')
+btn = st.button('Analyze')
+if btn == True:
+    res1 = nlp(ans)
+    res2 = nlp(sub)
+    sim = res1.similarity(res2)
+    st.write("Similarity Score: ", sim)
+    #scale the similarity score to 10
+    sim = m.ceil(sim*10)
+    st.write("Scaled Score: ", sim)
